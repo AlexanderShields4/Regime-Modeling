14c5b6f7dd804fffcd7b7e4712139c237b50c0a6
jackbray287 <jabray@davidson.edu>
2025-10-23 15:46:20 +0200

    another

--- __main__.py
+++ __main__.py
@@ -1,7 +1,10 @@
 import streamlit as st
 import pandas as pd
 
-
+# Regime-Modeling/__main__.py
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 # Import data loaders
 from data.ind_stocks import load_individual_stocks
