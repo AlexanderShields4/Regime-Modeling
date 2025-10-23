345beab6373b966a2c5eef91265df73d2de66f09
jackbray287 <jabray@davidson.edu>
2025-10-23 18:51:07 +0200

    done for the day

--- requirements-check.py
+++ requirements-check.py
@@ -1,9 +1,12 @@
 import yfinance
 import pandas as pd
-import streamlit
-import numpy as np 
-from hhmlearn.hmm import MultinomialHMM
+import streamlit as st
+import numpy as np
+import hmmlearn
+from hmmlearn.hmm import MultinomialHMM
 
 print("yfinance:", yfinance.__version__)
 print("pandas:", pd.__version__)
-print("streamlit:", streamlit.__version__)
+print("streamlit:", st.__version__)
+print("numpy:", np.__version__)
+print("hmmlearn:", hmmlearn.__version__)
