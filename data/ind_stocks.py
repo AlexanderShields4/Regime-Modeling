6143b52efc9263175549497b90db589bef4cdcc0
jackbray287 <jabray@davidson.edu>
2025-10-23 15:16:26 +0200

    more setup

---
+++ data/ind_stocks.py
@@ -0,0 +1,5 @@
+from data_utils import get_individual_stocks
+
+def load_individual_stocks(period="1y", interval="1d"):
+    df = get_individual_stocks(period=period, interval=interval)
+    return df
