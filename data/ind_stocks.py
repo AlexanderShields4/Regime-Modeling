a9906817fe28b058009321220e2314ec63a4d9a4
Alex <alshields1@davidson.edu>
2025-11-20 15:54:03 +0100

    changed the window from 1 year to 15 for model accuracy

--- data/ind_stocks.py
+++ data/ind_stocks.py
@@ -1,7 +1,7 @@
 # data/ind_stocks.py
 from .data_utils import get_individual_stocks
 
-def load_individual_stocks(tickers=None, period="1y", interval="1d"):
+def load_individual_stocks(tickers=None, period="15y", interval="1d"):
     """Wrapper around data_utils function for consistency."""
     return get_individual_stocks(tickers=tickers, period=period, interval=interval)
 
--- data/indices.py
+++ data/indices.py
@@ -1,5 +1,5 @@
 from .data_utils import get_indices
 
-def load_indices(period="1y", interval="1d"):
+def load_indices(period="15y", interval="1d"):
     df = get_indices(period=period, interval=interval)
     return df
